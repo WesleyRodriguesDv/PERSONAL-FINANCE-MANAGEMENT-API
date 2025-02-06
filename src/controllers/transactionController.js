d12deefb3effe463badf2fb3a1aadaf677507543
Wesley RodriguesDv <wesleyrodriiguesdev@gmail.com>
2025-02-06 09:35:44 -0300

    Regras de Negócio Transações

---
+++ src/controllers/transactionController.js
@@ -0,0 +1,17 @@
+import {criarTransacao, listarTransacoesUsuario, listrarTransacoesUsusuario} from '../models/transactionModel.js'
+
+export const adicionarTransacao = async (req,res) =>{
+    const{tipo, categoria, valor} = req.body
+
+    const usuarioId = req.usuarioId
+
+    const transacao = await criarTransacao({tipo, categoria, valor, usuarioId});
+
+    res.status(201).json(transacao)
+};
+
+export const listarTransacoes = async (req,res) =>{
+    const transacoes = await listarTransacoesUsuario(req.usuarioId);
+    res.json(transacoes)
+};
+
