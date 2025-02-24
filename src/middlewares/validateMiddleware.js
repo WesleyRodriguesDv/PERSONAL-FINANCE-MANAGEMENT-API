681a9f400cf5b1a962a4f8e2452de5d2e2089e8f
Wesley RodriguesDv <wesleyrodriiguesdev@gmail.com>
2025-02-24 16:55:40 -0300

    correção validate

--- src/middlewares/validateMiddleware.js
+++ src/middlewares/validateMiddleware.js
@@ -1,4 +1,4 @@
-/*export const validarCadastro = (req, res, next) => {
+export const validarCadastro = (req, res, next) => {
   const { email, senha } = req.body;
   if (!email || !senha) {
     return res.status(400).json({ erro: "Todos os campos são obrigatórios" });
@@ -16,27 +16,4 @@ export const validarTransacao = (req, res, next) => {
     return res.status(400).json({ erro: "Tipo deve ser 'receita' ou 'despesa'." });
   }
   next();
-};*/
-
-
-
-export const validarCadatro = (req, res, next) =>{
-  const {email, senha} = req.body
-  if (!email || !senha) {
-    return res.status(400).json({erro:"Todos os campos são obrigatórios"})
-  }
-  next()
-}
-
-export const validarTransacao = (req, res, next) =>{
-  const{tipo, categoria, valor} = req.body
-
-  if (!tipo, !categoria, !valor) {
-    return res.status(400).json({erro: "Todos os campos são obrigatórios"})
-  }
-
-  if (tipo !== "receita" && tipo !== "despesa") {
-    return res.status(400).json({erro: "Tipo deve ser 'receita' ou 'despesa' "})
-  }
-next()
-}
+};
