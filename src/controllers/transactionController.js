a781b21a4269c466240c9128f140d2427973ebc4
Wesley RodriguesDv <wesleyrodriiguesdev@gmail.com>
2025-02-24 10:53:55 -0300

    correção do código em transactionController

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,17 +1,51 @@
-import {criarTransacao, listarTransacoesUsuario} from '../models/transactionModel.js'
 
-export const adicionarTransacao = async (req,res) =>{
-    const{tipo, categoria, valor} = req.body
+import { criarTransacao, listarTransacoesUsuario } from "../models/transactionModel.js";
+
+export const adicionarTransacao = async (req, res) =>{
+    const {tipo, categoria, valor} = req.body
 
     const usuarioId = req.usuarioId
 
-    const transacao = await criarTransacao({tipo, categoria, valor, usuarioId});
-
+    const transacao = await criarTransacao({tipo, categoria, valor, usuarioId})
     res.status(201).json(transacao)
-};
+}
 
 export const listarTransacoes = async (req,res) =>{
-    const transacoes = await listarTransacoesUsuario(req.usuarioId);
+    const transacoes = await listarTransacoes(usuarioId)
     res.json(transacoes)
-};
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
