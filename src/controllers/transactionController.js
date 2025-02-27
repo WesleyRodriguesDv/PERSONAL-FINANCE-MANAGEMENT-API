68c54cb2cb31aa96870b4df7df5b2e3ec88eebeb
Wesley RodriguesDv <wesleyrodriiguesdev@gmail.com>
2025-02-27 10:53:32 -0300

    adicionando função para visualizar o saldo atual em cada transação

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,20 +1,30 @@
 
 import { criarTransacao, listarTransacoesUsuario } from "../models/transactionModel.js";
+import { obterSaldo } from "../models/transactionModel.js";
+
 
 export const adicionarTransacao = async (req, res) =>{
     const {tipo, categoria, valor, descricao} = req.body
-
     const usuarioId = req.usuarioId
-
-    const transacao = await criarTransacao({tipo, categoria, valor, descricao, usuarioId})
-    res.status(201).json(transacao)
+''
+    try{
+        const transacao = await criarTransacao({tipo, categoria, valor, descricao, usuarioId})
+        const saldo = await obterSaldo (usuarioId)
+        res.status(201).json({transacao, saldo})
+    } catch(error) {
+        return res.status(400).json({erro: "Erro ao efetuar transação"})
+    }
 }
+
 
 export const listarTransacoes = async (req,res) =>{
     const transacoes = await listarTransacoesUsuario(req.usuarioId)
-    res.json(transacoes)
-
+    res.json({transacoes})
 }
+
+
+
+
 
 
 
