60e9c1e410197aa52412e5d3e5d4a620bb936f9f
Wesley RodriguesDv <wesleyrodriiguesdev@gmail.com>
2025-03-25 11:25:40 -0300

    build

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express from "express";
 import userRoutes from "./routes/public/userRoutes.js";
 import transactionRoutes from "./routes/private/transactionRoutes.js";
-
+import cors from "cors"
 const app = express();
 app.use(express.json());
 
 
-
+app.use(cors());
 app.use("/", userRoutes);
 app.use("/", transactionRoutes);
 
